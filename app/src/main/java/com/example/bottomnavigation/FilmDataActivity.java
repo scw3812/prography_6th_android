8967d01482eb761e27e051e731047e345fa95ae3
scw3812 <dlffpraos@naver.com>
2020-03-02 18:31:30 +0900

    modify design

--- app/src/main/java/com/example/bottomnavigation/FilmDataActivity.java
+++ app/src/main/java/com/example/bottomnavigation/FilmDataActivity.java
@@ -17,9 +17,9 @@ public class FilmDataActivity extends AppCompatActivity {
         FilmData filmData = (FilmData)getIntent().getSerializableExtra("filmData");
         textView = (TextView)findViewById(R.id.film_info);
 
-        String film_info = filmData.getTitle() + "\n설명 : " + filmData.getDescription() +
-                "\n감독 : " + filmData.getDirector() + "\n제작자 : " + filmData.getProducer() +
-                "\n출시년도 : " + filmData.getReleaseDate() +"\n평점 : " + filmData.getRtScore();
+        String film_info = filmData.getTitle() + "\n\n설명 : " + filmData.getDescription() +
+                "\n\n감독 : " + filmData.getDirector() + "\n\n제작자 : " + filmData.getProducer() +
+                "\n\n출시년도 : " + filmData.getReleaseDate() +"\n\n평점 : " + filmData.getRtScore();
         textView.setText(film_info);
     }
 }
